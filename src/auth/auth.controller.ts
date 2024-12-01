ccc964b55d34eaffd5dc33504a773ca117f2c090
OriHaguel <orihaguel@gmail.com>
2024-12-01 23:55:57 +0200

    Modify url

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -5,7 +5,7 @@ import { AuthService } from './auth.service';
 import { LocalAuthGuard } from './guards/local-auth-guard';
 import { JwtAuthGuard } from './guards/jwt-auth-guard';
 
-@Controller('auth')
+@Controller('api/auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) { }
 
